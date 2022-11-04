8cf388b3e317ceb58f72d536952010a5340858b1
syhway <1461507176@qq.com>
2022-11-04 21:39:01 +0800

    你大爷的 master commit1

--- src/main/java/hehe/Gittest.java
+++ src/main/java/hehe/Gittest.java
@@ -6,5 +6,7 @@ public class Gittest {
         System.out.println("hello Git2");
         System.out.println("hello Git3");
         System.out.println("hello Git4");
+        System.out.println("hotfix test");
+        System.out.println("master test");
     }
 }
